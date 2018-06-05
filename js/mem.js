fb6ba2cd6cd6fc601bccf4ee1aeaf1bce39ed341
hermelen <Hermelen>
2018-06-05 12:30:39 +0200

    JS different

--- js/mem.js
+++ js/mem.js
@@ -1,25 +1,34 @@
+
+
 var divList = document.getElementsByTagName('div');
 var rectoList = document.getElementsByClassName('recto');
 var versoList = document.getElementsByClassName('verso');
-var src = "";
+var prevSrc = "";
+var prevClass = [];
 for (var i = 0; i < divList.length; i++) {
     div = divList[i];
     div.addEventListener('click', function(){
         recto = this.childNodes[1];
         verso = this.childNodes[3];
-        if (recto.src == src) {
-            recto.style.display = "block";
-            verso.style.display = "none";
-        }
-        else {
-            
-        }
         verso.style.display = "none";
         recto.style.display = "block";
-        src = recto.src;
+        prevRecto = document.getElementsByClassName(prevClass);
+        prevRecto_0 = prevRecto[0];
+        prevRecto_1 = prevRecto[1];
+        if (prevRecto_0 != null && recto.src != prevSrc) {
+            console.log('different');
+            // if (prevRecto_0 != null) {
+              console.log(prevRecto_0);
+              prevRecto_0.style.display = "none";
+            // }
+            // prevRecto_1.style.display = "none";
+        } else {
+          console.log('idem');
+        }
 
-        console.log(src);
-})
+        prevSrc = recto.src;
+        prevClass = recto.className;
+    })
 }
 
 
